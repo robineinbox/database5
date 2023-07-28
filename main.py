574ca98df545884b885f896bcce4cdc9d7e21ae2
robineinbox <robin_e@inbox.ru>
2023-07-28 19:02:09 +0300

    ..

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db_manager import DBManager
 def run_main():
     companies = ['1740', '2180', '3529', '3112647', '362', '3093544', '22494', '78638', '54', '87021']
     db_manager = DBManager()
+    unique_employers = set()
     for company in companies:
         url = f'https://api.hh.ru/vacancies?employer_id={company}'
         response = requests.get(url)
@@ -17,14 +18,14 @@ def run_main():
                     salary = None
                 vacancy_name = vacancy['name']
                 vacancy_link = vacancy['alternate_url']
-
                 # Insert employer data into database
                 employer = vacancy.get('employer')
                 employer_name = employer.get('name') if employer else None
                 employer_description = employer.get('description') if employer else None
-                db_manager.insert_employer(employer_name, employer_description)
+                if employer_name not in unique_employers:
+                    db_manager.insert_employer(employer_name, employer_description)
+                    unique_employers.add(employer_name)
                 db_manager.insert_vacancy(company, vacancy_name, salary, vacancy_link)
-
         else:
             print(f'Ошибка при получении данных о компании {company}. Статус-код:', response.status_code)
 
